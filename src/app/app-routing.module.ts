afda72bf5f7728980ac38c7178ad8157079e72e5
matanad <matanad3@gmail.com>
2025-03-17 19:38:02 +0200

    first structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }, { path: 'books', loadChildren: () => import('./modules/books/books.module').then(m => m.BooksModule) }, { path: 'cart', loadChildren: () => import('./modules/cart/cart.module').then(m => m.CartModule) }, { path: 'account', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule) }, { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'books',
+    loadChildren: () =>
+      import('./modules/books/books.module').then((m) => m.BooksModule),
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import('./modules/cart/cart.module').then((m) => m.CartModule),
+  },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./modules/account/account.module').then((m) => m.AccountModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
