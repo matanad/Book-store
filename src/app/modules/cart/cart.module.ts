484a7d9023dc9e3b02d0daf444928add309b1d30
matanad <matanad3@gmail.com>
2025-03-27 14:23:00 +0200

    feat: remove CartViewComponent and clean up cart module

--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartComponent } from './cart.component';
-import { CartViewComponent } from './components/cart-view/cart-view.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [CartComponent, CartViewComponent],
+  declarations: [CartComponent,],
   imports: [CommonModule, CartRoutingModule, SharedModule],
 })
 export class CartModule {}
