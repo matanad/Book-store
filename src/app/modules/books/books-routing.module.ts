ad2aecf96099767b3ab7cf291824882112112ff9
matanad <matanad3@gmail.com>
2025-03-27 14:21:00 +0200

    feat: restructure book routing, update book component templates, and enhance book details display logic

--- src/app/modules/books/books-routing.module.ts
+++ src/app/modules/books/books-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BooksComponent } from './books.component';
 
 const routes: Routes = [
-  { path: '', component: BookListComponent },
-  { path: ':id', component: BookDetailsComponent },
+  {
+    path: '',
+    component: BooksComponent,
+    children: [
+      { path: 'books', component: BookListComponent },
+      { path: 'book/:id', component: BookDetailsComponent },
+      {
+        path: '**',
+        redirectTo: 'books',
+        pathMatch: 'full',
+      },
+    ],
+  },
 ];
 
 @NgModule({
